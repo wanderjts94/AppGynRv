98d01364a23fa81f0296e37a5a65667e0641ac90
wanderjts94 <jhontitosalcedo94@gmail.com>
2024-03-15 19:39:00 -0500

    primer commmit tito

---
+++ app/src/main/java/pe/edu/idat/appgynrv/RecuperacionActivity.kt
@@ -0,0 +1,11 @@
+package pe.edu.idat.appgynrv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecuperacionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recuperacion)
+    }
+}
