da835651ab65578be6b08563d2c20681527a40c0
wanderjts94 <jhontitosalcedo94@gmail.com>
2024-03-30 16:44:47 -0500

    repoT

---
+++ app/src/main/java/pe/edu/idat/appgynrv/Retrofit/models/ejercicios/getlistaejercicioResponse.kt
@@ -0,0 +1,8 @@
+package pe.edu.idat.appgynrv.Retrofit.models.ejercicios
+
+data class getlistaejercicioResponse(
+    val nombreejercicio: String,
+    val cantidad: String,
+    val imagenes: String
+
+)
